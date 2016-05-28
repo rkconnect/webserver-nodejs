717b104bc7f5aef4ee1a77a1d11eeab78233b2c7
Rohit Kumar <rkconnect26@gmail.com>
2016-05-28 19:06:12 +0900

    Webserver get the file path from url and display the required html page. If that page is not available, it shows not found msg. If its a directory, its redirected to the Index.html

--- server.js
+++ server.js
@@ -2,11 +2,59 @@
  * Created by Rohit on 5/28/2016.
  */
 
+
+//Modules
 var http = require ('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+
+
+//MIME TYPE
+var mimeTypes = {
+    'html'  :   'text/html',
+    'jpeg'  :   'image/jpeg',
+    'jpg'   :   'image/jpg',
+    'png'   :   'image/png',
+    'js'    :   'text/javascript',
+    'css'   :   'text/css'
+};
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Context-Type': 'text/plain'});
-    res.end('Hello World!!\n');
+    //relative file path
+    var uri = url.parse(req.url).pathname;
+    //console.log(uri);
+
+    // absolute file path
+    var fileName = path.join(process.cwd(),uri);
+
+    //console.log(fileName);
+
+    //check if the file is available
+    var stats;
+    try{
+        stats = fs.lstatSync(fileName);
+
+        //Check if its a file or directory.
+        //Redirect to Index.html if its a directory
+        if(stats.isFile()){
+            var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
+            res.writeHead(200, {'Content-Type': mimeType});
+            fs.createReadStream(fileName).pipe(res);
+        }else if (stats.isDirectory()){
+            res.writeHead(302, {'Location': '/html/index.html'});
+            res.end();
+        }else {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.write('Error 500\n Something Went Wrong!!');
+            res.end();
+        }
+    }catch (e){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Error 404\n Page Not Found!!');
+        res.end();
+    }
+
 }).listen(8888);
 
 console.log('Server is running on port 8888');
